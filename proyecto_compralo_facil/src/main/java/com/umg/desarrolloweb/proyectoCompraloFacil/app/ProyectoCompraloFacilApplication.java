2f1b95081c37e93070295c77749d5be37b93b321
Ubuntu <ubuntu@ip-172-31-0-64.us-east-2.compute.internal>
2020-10-16 20:49:24 +0000

    Commit inicial del proyecto con las dependencias necesarias por el momento, pre a trabajar en equipo

---
+++ proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/ProyectoCompraloFacilApplication.java
@@ -0,0 +1,13 @@
+package com.umg.desarrolloweb.proyectoCompraloFacil.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCompraloFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCompraloFacilApplication.class, args);
+	}
+
+}
---
+++ proyecto_compralo_facil/src/test/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/ProyectoCompraloFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.desarrolloweb.proyectoCompraloFacil.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCompraloFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
