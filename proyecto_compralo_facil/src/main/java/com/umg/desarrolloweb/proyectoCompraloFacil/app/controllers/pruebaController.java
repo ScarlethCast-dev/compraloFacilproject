cbb477cec88ce5402cdd9a29a3eafc9d2d4f63b0
Scarleth Castillo <heidy.20mendez@gmail.com>
2020-10-17 11:05:14 -0600

    quitando h1

--- proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/controllers/pruebaController.java
+++ proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/controllers/pruebaController.java
@@ -10,7 +10,7 @@ public class pruebaController{
 
 	@GetMapping("/hola")
 	public String prueba(){
-		return "<h1> Probando el funcionamiento con el repositorio en GIT 22222 2.0</h1>";
+		return "<Probando el funcionamiento con el repositorio en GIT 22222 2.0";
 	}
 	
 	@GetMapping("/hola1")
