a1c9e0e306620899e4faaf026ee4201adbb6dfb9
Mynor Solis <msolism7@miumg.edu.gt>
2020-10-18 11:38:53 -0600

    Union de la rama con la clase abstracta

---
+++ proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/entities/AbstractEntity.java
@@ -0,0 +1,27 @@
+package com.umg.desarrolloweb.proyectoCompraloFacil.app.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embedded;
+import javax.persistence.MappedSuperclass;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@MappedSuperclass
+public class AbstractEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1263876128L;
+	@JsonIgnore
+	@Embedded
+	private Audit audit;
+	public Audit getAudit() {
+		return audit;
+	}
+	public void setAudit(Audit audit) {
+		this.audit = audit;
+	}
+
+}
---
+++ proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/entities/CLIENTE.java
@@ -0,0 +1,5 @@
+package com.umg.desarrolloweb.proyectoCompraloFacil.app.entities;
+
+public class CLIENTE {
+
+}
