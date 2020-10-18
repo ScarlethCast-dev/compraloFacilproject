baef180c43d7963d767ab91c7dc92902fecc5c9f
Scarleth Castillo <heidy.20mendez@gmail.com>
2020-10-18 11:36:10 -0600

    entidad producto creada

--- proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/entities/PRODUCTO.java
+++ proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/entities/PRODUCTO.java
@@ -1,5 +1,64 @@
 package com.umg.desarrolloweb.proyectoCompraloFacil.app.entities;
 
-public class PRODUCTO {
+import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="tProductos")
+public class PRODUCTO {
+private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private Long cantidad;
+    private BigDecimal precio;
+    private String foto;
+    
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Long getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Long cantidad) {
+		this.cantidad = cantidad;
+	}
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+    
 }
