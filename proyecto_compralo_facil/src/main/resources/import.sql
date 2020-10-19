2d47d197e9fed11dd98a0cab4af0e4b7e9a86af7
Diego Raymundo <diegor_137@outlook.es>
2020-10-19 13:37:51 -0600

    Pruebas e insercion de direcciones

--- proyecto_compralo_facil/src/main/resources/import.sql
+++ proyecto_compralo_facil/src/main/resources/import.sql
@@ -8,16 +8,30 @@ INSERT INTO t_clientes(ts_created, ts_modified, usu_created, usu_modified, nombr
 
 /*Municipios*/
 INSERT INTO t_municipio(ts_created, ts_modified, usu_created, usu_modified, nombre_municipio) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', 'Nebaj');
-INSERT INTO t_municipio(ts_created, ts_modified, usu_created, usu_modified, nombre_municipio) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', 'Cotzal');
+INSERT INTO t_municipio(ts_created, ts_modified, usu_created, usu_modified, nombre_municipio) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', 'Aguacatan');
+INSERT INTO t_municipio(ts_created, ts_modified, usu_created, usu_modified, nombre_municipio) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', 'Huehetenango');
 INSERT INTO t_municipio(ts_created, ts_modified, usu_created, usu_modified, nombre_municipio) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', 'Uspantan');
+INSERT INTO t_municipio(ts_created, ts_modified, usu_created, usu_modified, nombre_municipio) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', 'Ixtahuacan');
 
 /*Departamento*/
 
 INSERT INTO t_departamento(ts_created, ts_modified, usu_created, usu_modified, nombre_depart) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', 'Quiche');
+INSERT INTO t_departamento(ts_created, ts_modified, usu_created, usu_modified, nombre_depart) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', 'Huehuetenango');
 
 /*Direcciones clientes*/
 
 INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 1 2-2', 'null', 1, 1, 1);
-INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 2 2-2', 'null', 1, 2, 1);
-INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 3 2-2', 'null', 1, 3, 4);
+INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 2 2-2', 'null', 1, 1, 1);
+
+INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 3 2-2', 'null', 2, 2, 2);
+INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 4 2-2', 'null', 2, 2, 2);
+
+INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 5 2-2', 'null', 2, 3, 3);
+INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 6 2-2', 'null', 2, 3, 3);
+
+INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 7 2-2', 'null', 1, 4, 4);
+INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 8 2-2', 'null', 1, 4, 4);
+
+INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 9 2-2', 'null', 2, 5, 5);
+INSERT INTO t_direcciones(ts_created, ts_modified, usu_created, usu_modified, codigo_postal, desc_direccion, cliente_id, departamento_id, municipio_id, t_cliente_id) values ('1999-12-31 23:59:59','1999-12-31 23:59:59','Jefe','Jefe', '14013', 'Zona 10 2-2', 'null', 2, 5, 5);
 
