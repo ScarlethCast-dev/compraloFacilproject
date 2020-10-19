3d61cfe02e04b3681996c0f92f23dd6f58d09acf
Scarleth Castillo <heidy.20mendez@gmail.com>
2020-10-18 21:28:34 -0600

    regresando tcuenta a la normalidad

--- proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/entities/TCuenta.java
+++ proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/entities/TCuenta.java
@@ -2,8 +2,6 @@ package com.umg.desarrolloweb.proyectoCompraloFacil.app.entities;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -45,12 +43,6 @@ public class TCuenta extends AbstractEntity implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="id_estado_cuenta", insertable = false, updatable = false)
 	private TEstadoCuenta tEstadoCuenta;
-	
-	@OneToMany(mappedBy = "tCliente")
-    private List<TListaDeseos>tListaDeseo;
-   
-    @OneToMany(mappedBy = "tCliente")
-    private List<THistorial>tHistorial;
 	
 	public Long getIdCuenta() {
 		return idCuenta;
