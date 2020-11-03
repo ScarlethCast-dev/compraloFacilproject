98335049a05a46c9f684a7dcc4b9afa32c1943e7
Diego Raymundo <diegor_137@outlook.es>
2020-11-02 21:17:34 -0600

    Se mejoro el carrito

--- proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/controllers/CarritoController.java
+++ proyecto_compralo_facil/src/main/java/com/umg/desarrolloweb/proyectoCompraloFacil/app/controllers/CarritoController.java
@@ -27,7 +27,7 @@ public class CarritoController {
 
 	@PostMapping("/carrito")
 	public String ingresar(TDetallePedido tDetallePedido,  Model model) {
-		
+	
 		TDetallePedido det = new TDetallePedido();
 		
 		det.setIdDetallePedido(tDetallePedido.getIdDetallePedido());
@@ -41,7 +41,7 @@ public class CarritoController {
 		return "redirect:/carrito";	
 	}
 	
-	@GetMapping(value = "/eliminar-producto/{id}") 
+	@GetMapping(value = "/eliminar-producto-carrito/{id}") 
 	public String eliminar(@PathVariable(value = "id") Long id){
 		for (int i = 0; i < lista.size(); i++) {
 			if(Objects.equals(lista.get(i).getIdDetallePedido(), id)) {
@@ -62,6 +62,53 @@ public class CarritoController {
 		return "carrito/carrito-lista";
 		
 	}
+	
+	@GetMapping(value = "/cambiar-suma/{id}") 
+	public String sumar(@PathVariable(value = "id") Long id){
+		TDetallePedido det = new TDetallePedido();
+		Long cantidadSum;
+		for (int i = 0; i < lista.size(); i++) {
+			if(Objects.equals(lista.get(i).getIdDetallePedido(), id)) {
+				cantidadSum=lista.get(i).getCantidad() + 1;
+				det.setIdDetallePedido(id);
+				det.setLinkProducto(lista.get(i).getLinkProducto());
+				det.setDetalleProducto(lista.get(i).getDetalleProducto());
+				det.setPrecio(lista.get(i).getPrecio());
+				det.setCantidad(cantidadSum);
+				
+				this.lista.set(i, det);
+				
+			}
+		}
+		return "redirect:/carrito";
+	}
+	
+	@GetMapping(value = "/cambiar-resta/{id}") 
+	public String restar(@PathVariable(value = "id") Long id){
+		TDetallePedido det = new TDetallePedido();
+		Long cantidadSum;
+		for (int i = 0; i < lista.size(); i++) {
+			if(Objects.equals(lista.get(i).getIdDetallePedido(), id)) {
+				
+				cantidadSum=lista.get(i).getCantidad() - 1;
+				det.setIdDetallePedido(id);
+				det.setLinkProducto(lista.get(i).getLinkProducto());
+				det.setDetalleProducto(lista.get(i).getDetalleProducto());
+				det.setPrecio(lista.get(i).getPrecio());
+				det.setCantidad(cantidadSum);
+				
+				this.lista.set(i, det);
+				
+				if (cantidadSum==0) {
+					return "redirect:/eliminar-producto-carrito/"+id;
+				}
+				
+			}
+		}
+		return "redirect:/carrito";
+	}
+	
+	
 	
 	public void total(){
 		total=0;
